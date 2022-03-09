ac28eb36c30278b9533406e2377198412b5feec1
Pasqua <chideraemeka2@gmail.com>
2022-03-08 17:00:09 -0800

    added

---
+++ pages/post.js
@@ -0,0 +1,34 @@
+import Nav from "../components/Nav";
+
+const Post = () => {
+    return (
+        <div>
+            <Nav />
+            <div className="flex flex-col my-4 w-full max-x-[85%] mx-auto">
+                <h2 className="text-xl text-black my-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae iaculis nisi.</h2>
+                <div className="w-full flex flex-col items-center my-2">
+                    <img src="/images/car.png" className="w-full" />
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae iaculis nisi. Praesent varius diam nisi, sit amet mollis dolor pharetra sit amet. Suspendisse porttitor viverra nunc nec ultrices. Nam venenatis quis massa at tempus. Suspendisse pretium metus magna, et interdum dolor gravida luctus. Fusce maximus nisi eros, eu malesuada ipsum ultrices vel. Nam in mi finibus, venenatis nisi tempor, feugiat massa. 
+                    Quisque elementum, mi vel sodales luctus, est nunc egestas tortor, eget accumsan sapien lorem vitae erat. Curabitur a ex iaculis, posuere lorem at, varius urna. Nam iaculis viverra eros porta fringilla. Nulla vulputate, orci eu convallis rutrum, metus felis euismod quam, quis faucibus mauris dolor eget quam. Proin facilisis erat nunc, quis placerat erat tempor ut. Vestibulum fermentum a ligula id faucibus.
+                    Pellentesque maximus ipsum nunc, eleifend congue nisl faucibus ut. Nulla ultricies augue
+                     ut viverra congue. Maecenas arcu metus, posuere sed orci ut, convallis viverra eros. Proin in neque nisi. Proin eu nunc fringilla, dapibus nisi nec, pretium felis. Sed aliquam dui est, auctor egestas turpis dictum a. Donec placerat eu orci eget cursus. Mauris sodales iaculis mauris et feugiat. In aliquam mi lacinia massa egestas rutrum.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae iaculis nisi. Praesent varius diam nisi, sit amet mollis dolor pharetra sit amet. Suspendisse porttitor viverra nunc nec ultrices. Nam venenatis quis massa at tempus. Suspendisse pretium metus magna, et interdum dolor gravida luctus. Fusce maximus nisi eros, eu malesuada ipsum ultrices vel. Nam in mi finibus, venenatis nisi tempor, feugiat massa. 
+                    Quisque elementum, mi vel sodales luctus, est nunc egestas tortor, eget accumsan sapien lorem vitae erat. Curabitur a ex iaculis, posuere lorem at, varius urna. Nam iaculis viverra eros porta fringilla. Nulla vulputate, orci eu convallis rutrum, metus felis euismod quam, quis faucibus mauris dolor eget quam. Proin facilisis erat nunc, quis placerat erat tempor ut. Vestibulum fermentum a ligula id faucibus.
+                    Pellentesque maximus ipsum nunc, eleifend congue nisl faucibus ut. Nulla ultricies augue
+                     ut viverra congue. Maecenas arcu metus, posuere sed orci ut, convallis viverra eros. Proin in neque nisi. Proin eu nunc fringilla, dapibus nisi nec, pretium felis. Sed aliquam dui est, auctor egestas turpis dictum a. Donec placerat eu orci eget cursus. Mauris sodales iaculis mauris et feugiat. In aliquam mi lacinia massa egestas rutrum.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae iaculis nisi. Praesent varius diam nisi, sit amet mollis dolor pharetra sit amet. Suspendisse porttitor viverra nunc nec ultrices. Nam venenatis quis massa at tempus. Suspendisse pretium metus magna, et interdum dolor gravida luctus. Fusce maximus nisi eros, eu malesuada ipsum ultrices vel. Nam in mi finibus, venenatis nisi tempor, feugiat massa. 
+                    Quisque elementum, mi vel sodales luctus, est nunc egestas tortor, eget accumsan sapien lorem vitae erat. Curabitur a ex iaculis, posuere lorem at, varius urna. Nam iaculis viverra eros porta fringilla. Nulla vulputate, orci eu convallis rutrum, metus felis euismod quam, quis faucibus mauris dolor eget quam. Proin facilisis erat nunc, quis placerat erat tempor ut. Vestibulum fermentum a ligula id faucibus.
+                    Pellentesque maximus ipsum nunc, eleifend congue nisl faucibus ut. Nulla ultricies augue
+                     ut viverra congue. Maecenas arcu metus, posuere sed orci ut, convallis viverra eros. Proin in neque nisi. Proin eu nunc fringilla, dapibus nisi nec, pretium felis. Sed aliquam dui est, auctor egestas turpis dictum a. Donec placerat eu orci eget cursus. Mauris sodales iaculis mauris et feugiat. In aliquam mi lacinia massa egestas rutrum.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Post;
