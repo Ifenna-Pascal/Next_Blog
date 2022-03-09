5bc015030a86cccf431c9f097abfd4ec7e243750
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-03-09 21:17:30 +0100

    added

--- pages/post.js
+++ pages/post.js
@@ -58,6 +58,10 @@ const CommentSection = () => {
     const formChange = (e) => {
         setForm({...form, [e.target.name]:e.target.value})
     }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(form);
+    }
     return (
         <div className="my-4 max-w-[85%]">
         <h1 className="text-3xl font-bold text-[#00B0F0] tracking-wide mb-1">Reader Comments</h1>
@@ -69,18 +73,18 @@ const CommentSection = () => {
             <div className="w-full max-w-[95%] bg-white rounded-sm px-8 my-8 py-12">
                 <h1 className="font-semibold mb-4">Join the discussion</h1>
                 <form onSubmit={handleSubmit} className="flex w-[85%] flex-col">
-                    <input type="text" placeholder="Write your comment" value={form.comment} name="comment" className="outline-none bg-transparent rounded-3xl border border-gray-200 w-full py-4 px-4 mb-6" />
+                    <input type="text" placeholder="Write your comment" onChange={formChange} value={form.comment} name="comment" className="outline-none bg-transparent rounded-3xl border border-gray-200 w-full py-4 px-4 mb-6" />
                     <div className="grid grid-cols-2 gap-x-5 gap-y-5 mb-4 w-[70%]">
                         <div className="basis-[50%]">
                             <span className="text-base block traking-wide mb-2">Your Name</span>
-                            <input className="bg-transparent w-full py-2 border border-gray-200  rounded-2xl" />
+                            <input className="bg-transparent w-full py-2 border border-gray-200  rounded-2xl" name="last_name"  value={form.last_name} onChange={formChange} />
                         </div>
                         <div className="basis-[50%]">
                             <span className="text-base block traking-wide mb-2">Your Name</span>
-                            <input name="first_name" value={form.first_name} className="bg-transparent w-full py-2 border border-gray-200  rounded-2xl" />
+                            <input name="first_name"  value={form.first_name} onChange={formChange} className="bg-transparent w-full py-2 border border-gray-200  rounded-2xl" />
                         </div>
                         <div className="flex flex-col col-span-1 items-center">
-                            <button name="last_name" value={form.last_name} className="text-white bg-[#00B0F0] rounded-3xl py-2 mt-6 border-none w-full">Submit</button>
+                            <button className="text-white bg-[#00B0F0] rounded-3xl py-2 mt-6 border-none w-full">Submit</button>
                         </div>
                     </div> 
                 </form>
